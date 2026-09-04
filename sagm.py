b7a3e9182c97d40eea7d9c25fb34861db9b35d99
XUAN QI <94367922+xuanqi99@users.noreply.github.com>
2026-09-04 14:47:56 +0200

    Update sagm.py

--- sagm.py
+++ sagm.py
@@ -13,6 +13,7 @@ from __future__ import annotations
 from __future__ import annotations
 from __future__ import annotations
 from __future__ import annotations
+from __future__ import annotations
 
 from dataclasses import dataclass
 from typing import Callable, Sequence, Type
