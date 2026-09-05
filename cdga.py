a22db3e9c6f0c95a62373a2a749bda3d6db0e744
XUAN QI <94367922+xuanqi99@users.noreply.github.com>
2026-09-05 17:25:53 +0200

    Update cdga.py

--- cdga.py
+++ cdga.py
@@ -15,18 +15,6 @@ from pathlib import Path
 import random
 import shutil
 from typing import Callable, Iterable, Mapping, Sequence
-from typing import Callable, Iterable, Mapping, Sequence
-from typing import Callable, Iterable, Mapping, Sequence
-from typing import Callable, Iterable, Mapping, Sequence
-from typing import Callable, Iterable, Mapping, Sequence
-from typing import Callable, Iterable, Mapping, Sequence
-from typing import Callable, Iterable, Mapping, Sequence
-from typing import Callable, Iterable, Mapping, Sequence
-from typing import Callable, Iterable, Mapping, Sequence
-from typing import Callable, Iterable, Mapping, Sequence
-from typing import Callable, Iterable, Mapping, Sequence
-from typing import Callable, Iterable, Mapping, Sequence
-
 
 IMAGE_EXTENSIONS = {".bmp", ".jpeg", ".jpg", ".png", ".tif", ".tiff", ".webp"}
 
